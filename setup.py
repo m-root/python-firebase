0cfa3f997952a9ee8cebd3c51fca18a97095b44d
m-root <muemakamba@gmail.com>
2018-07-11 05:48:25 +0300

    install_requires update

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author = 'Michael Huynh',
     author_email = 'mike@mikexstudios.com',
     url = 'http://github.com/mikexstudios/python-firebase',
-    install_requires = ['requests >=1.2.0,<1.2.99'], 
+    install_requires = ['requests >=2.11.1'], 
     classifiers = [
         'Programming Language :: Python', 
         'License :: OSI Approved :: BSD License',
